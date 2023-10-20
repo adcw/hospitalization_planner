cd207e3504aacedca7b65e489ed4470f5e58801a
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-10-20 14:13:34 +0200

    Evaluating model

--- src/train.py
+++ src/train.py
@@ -1,13 +1,11 @@
-import os
-
-import numpy as np
 import pandas as pd
 import torch
+import data.colnames as c
 
-from src.experimental.triple_arch.archs import StepTimeLSTM
 from src.experimental.triple_arch.state_predict_module import StatePredictionModule
-from src.preprocessing.clean_data import strip_data, create_header_constants, get_sequences
-from src.preprocessing.sequences import preprocess_data, make_sequences
+from src.preprocessing.preprocess import Preprocessor
+# from src.preprocessing.preprocess import preprocess_data
+from src.preprocessing.sequences import make_sequences
 
 DATA_RAW_PATH = "../data/neonatologia.txt"
 DATA_STRIPPED_PATH = "../data/neonatologia_stripped.txt"
@@ -17,12 +15,29 @@ COLNAMES_PATH = "../data/colnames.py"
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    sequences = get_sequences()
+    # if not os.path.isfile(data_path):
+    #     strip_data(DATA_STRIPPED_PATH, DATA_STRIPPED_PATH)
+    #
+    # if not os.path.isfile(COLNAMES_PATH):
+    #     create_header_constants(DATA_STRIPPED_PATH, COLNAMES_PATH)
+    #     return
+
+    # read data
+    whole_df = pd.read_csv(DATA_STRIPPED_PATH, sep=" ", dtype=object)
+
+    # preprocess: one hot, impute
+    onehot_cols = [c.POSIEW_SEPSA, c.UREOPLAZMA, c.RDS, c.TYPE_RDS, c.PDA, c.RESPCODE]
+
+    preprocessor = Preprocessor()
+    preprocessed_df = preprocessor.transform(whole_df, onehot_cols=onehot_cols, impute=True)
+
+    # create sequences
+    sequences = make_sequences(preprocessed_df)
 
     n_attr = sequences[0].shape[1]
 
     pred_model = StatePredictionModule(n_attr=n_attr, hidden_size=256, device=device)
-    pred_model.train(sequences=sequences, es_patience=2, epochs=30, n_splits=3)
+    pred_model.evaluate(sequences=sequences[:20], es_patience=2, epochs=30, n_splits=5)
 
     pass
 
