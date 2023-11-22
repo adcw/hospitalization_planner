6241e9663060dfc52f1953c0f139e1807bb2b75f
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-11-22 15:04:47 +0100

    Add plot_trajectories - visualize test data

--- src/session/helpers.py
+++ src/session/helpers.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
+from typing import List
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
@@ -29,12 +31,57 @@ def train_model_helper(
     return model
 
 
+def plot_trajectories(ts: List[np.iterable], title: str = "Trajectories"):
+    num_subplots = min(len(ts), 6 * 6)
+    num_cols = int(np.ceil(np.sqrt(num_subplots)))
+    num_rows = int(np.ceil(num_subplots / num_cols))
+
+    ts = ts.copy()
+
+    fig, axs = plt.subplots(num_rows, num_cols, figsize=(3 * num_rows, 3 * num_cols))
+
+    if len(ts) == 1:
+        axs.plot(ts[0])
+    else:
+        for i in range(num_rows):
+            for j in range(num_cols):
+                if len(ts) > 0:
+                    data = ts.pop(0)
+                    axs[i, j].plot(data, label=f'Series {i * num_cols + j + 1}')
+                    axs[i, j].legend()
+                else:
+                    axs[i, j].axis('off')
+
+    plt.tight_layout()
+    plt.suptitle(title)
+    plt.show()
+
+
+def plot_critical_places(seq: np.ndarray):
+    diff = np.diff(seq.ravel())
+    points = np.where(diff != 0)
+
+    trajectories = []
+
+    for point in points:
+        start = point[0] - 30
+        end = point[0] + 30
+
+        trajectories.append(seq[start: end])
+
+    plot_trajectories(trajectories)
+
+
 def test_model_helper(
         model_payload: ModelPayload,
         sequences: list[pd.DataFrame],
         limit: int = 15,
         offset: int = 0,
 ):
+    pass
+
+    plot_critical_places(sequences[2][model_payload.model_params.cols_predict].values)
+
     for seq in tqdm(sequences[offset:offset + limit], desc="Evaluating test cases"):
         split_point = round(0.5 * len(seq))
         input_sequence: pd.DataFrame = seq[:split_point]
