6b8f4dbb01c210b0a1c0ce626406a476eaf54960
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-11-28 16:01:08 +0100

    Refactor normalization.py a bit

--- src/preprocessing/normalization.py
+++ src/preprocessing/normalization.py
@@ -1,3 +1,5 @@
+from typing import Optional, List
+
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
@@ -21,19 +23,25 @@ def normalize_split(train_seq: list[np.ndarray], val_seq: list[np.ndarray] | Non
     train_seq_norm = np.split(train_cat_norm, np.cumsum([seq.shape[0] for seq in train_seq])[:-1])
 
     if val_seq is not None:
-        val_cat = np.concatenate(val_seq)
-        val_cat_norm = scaler.transform(val_cat)
-        val_seq_norm = np.split(val_cat_norm, np.cumsum([seq.shape[0] for seq in val_seq])[:-1])
+        val_seq_norm = transform_validation_data(val_seq, scaler)
     else:
         val_seq_norm = None
 
     return train_seq_norm, val_seq_norm, scaler
 
 
+def transform_validation_data(val_seq: Optional[List[np.ndarray]], scaler: MinMaxScaler):
+    val_cat = np.concatenate(val_seq)
+    val_cat_norm = scaler.transform(val_cat)
+    val_seq_norm = np.split(val_cat_norm, np.cumsum([seq.shape[0] for seq in val_seq])[:-1])
+
+    return val_seq_norm
+
+
 if __name__ == '__main__':
     df = pd.read_csv("../../data/clean/input.csv")
 
-    onehot_cols = [c.SEPSIS_CULTURE, c.RDS_TYPE]
+    onehot_cols = [c.POSIEW_SEPSA, c.TYPE_RDS]
 
     encoder = OneHotEncoder(onehot_cols)
 
