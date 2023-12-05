48ae38d1c9016a1647952a50c0f0b6f2410dfb8b
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-12-05 10:10:16 +0100

    set seeds

--- Main.py
+++ Main.py
@@ -2,7 +2,10 @@
 Use this script to train, validate and test models.
 """
 from src.session.ModelManager import ModelManager
+from src.utils.seeds import set_seed
 
 if __name__ == '__main__':
+    set_seed(9999)
+
     manager = ModelManager(models_dir='./models', config_path="./config.yaml")
     manager.start()
