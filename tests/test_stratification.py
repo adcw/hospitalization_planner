503870a35852ca163eeff0c708802a9c098ccb7a
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-12-22 17:27:15 +0100

    test_stratification.py

--- tests/test_stratification.py
+++ tests/test_stratification.py
@@ -1,17 +1,16 @@
-from src.model_selection.stratification import RegressionStratKFold
+from src.model_selection.regression_strat_kfold import RegressionStratKFold
+from src.model_selection.regression_train_test_split import RegressionTrainTestSplitter
 from src.session.model_manager import _get_sequences
 
 import numpy as np
+import pandas as pd
 
 from src.visualization.plot3d import scatter3d
 
 CSV_PATH = '../data/input.csv'
 
-if __name__ == '__main__':
-    sequences, _ = _get_sequences(CSV_PATH)
-    strat_col_indx = -1
-    strat_colname = sequences[0].columns.values[strat_col_indx]
 
+def test_kfold(sequences, strat_col_indx):
     kfold = RegressionStratKFold(n_clusters=7, n_splits=5, strat_col_indx=strat_col_indx)
     splits = kfold.split(sequences)
 
@@ -26,5 +25,45 @@ if __name__ == '__main__':
     splits_classes = np.array(splits_classes)
 
     scatter3d(features=splits_list, colors=splits_classes)
+
+
+def test_train_test_split(sequences, strat_col_indx):
+    # Perform split
+    splitter = RegressionTrainTestSplitter()
+    _, _ = splitter.fit_split(X=sequences, test_size=0.25, n_clusters=7, strat_col_index=strat_col_indx)
+
+    # Extract trajectory features and indices of train and test
+    features = splitter._features
+    train_indices = splitter._train_indices
+    test_indices = splitter._test_indices
+
+    entries = []
+    classes = [0 for _ in train_indices]
+    classes.extend([1 for _ in test_indices])
+
+    # Get features that correspond to train elements
+    for i in train_indices:
+        entry = features[i]
+        entries.append(entry)
+
+    # Get features that correspond to test elements
+    for i in test_indices:
+        entry = features[i]
+        entries.append(entry)
+
+    entries = np.stack(entries)
+
+    scatter3d(features=features, colors=splitter._clusters)
+    scatter3d(features=entries, colors=np.array(classes))
+
+    pass
+
+
+if __name__ == '__main__':
+    _sequences, _ = _get_sequences(CSV_PATH)
+    _strat_col_indx = -1
+
+    # test_kfold(_sequences, _strat_col_indx)
+    test_train_test_split(_sequences, _strat_col_indx)
 
     pass
