f7a8498d5be547115d21114c6ed19ca09ed9c7c2
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-12-27 13:54:00 +0100

    Change test_perc to smaller value

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from src.session.model_manager import ModelManager
 from src.config.seeds import set_seed
 
 if __name__ == '__main__':
-    set_seed(9999)
+    # set_seed(9999)
+    set_seed(1111)
 
-    manager = ModelManager(models_dir='./models', config_path="./config.yaml", test_perc=0.1)
+    manager = ModelManager(models_dir='./models', config_path="./config.yaml", test_perc=0.05)
     manager.start()
