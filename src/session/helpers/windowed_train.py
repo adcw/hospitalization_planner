2adba43536b2f69b0cae8f6e4e3a81c5a107f969
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-12-27 13:47:21 +0100

    Change to window size of 12

--- src/session/helpers/windowed_train.py
+++ src/session/helpers/windowed_train.py
@@ -9,7 +9,8 @@ def train_windowed_model(
         sequences: list[pd.DataFrame]
 ):
     model = WindowedModule(params=payload.model_params,
-                           n_attr_in=sequences[0].shape[1]
+                           n_attr_in=sequences[0].shape[1],
+                           window_size=12
                            )
     model.train(sequences=sequences, params=payload.train_params)
 
