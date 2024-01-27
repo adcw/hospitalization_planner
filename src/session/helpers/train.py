aafc6e31046ba176016c7adb7b601178a6c67bb6
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2024-01-27 13:17:24 +0100

    Make train helper create right model type

--- src/session/helpers/train.py
+++ src/session/helpers/train.py
@@ -9,15 +9,17 @@ def train_model(
         payload: SessionPayload,
         sequences: list[pd.DataFrame]
 ):
-    # model = StepModel(params=payload.model_params,
-    #                   n_attr_in=sequences[0].shape[1]
-    #                   )
-    # model.train(sequences=sequences, params=payload.train_params)
+    if payload.main_params.model_type == "step":
 
-    model = WindowModel(main_params=payload.main_params,
-                        n_attr_in=sequences[0].shape[1],
-                        window_size=12
-                        )
-    train_mae_losses, val_mae_losses = model.train(sequences=sequences, params=payload.train_params)
+        model = StepModel(main_params=payload.main_params,
+                          n_attr_in=sequences[0].shape[1]
+                          )
+        train_mae_losses, val_mae_losses = model.train(sequences=sequences, params=payload.train_params)
+    else:
+        model = WindowModel(main_params=payload.main_params,
+                            n_attr_in=sequences[0].shape[1],
+                            window_size=12
+                            )
+        train_mae_losses, val_mae_losses = model.train(sequences=sequences, params=payload.train_params)
 
     return model, (train_mae_losses, val_mae_losses)
