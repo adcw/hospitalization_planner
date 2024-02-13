e89b81a2ae05deed30aea20ee5228c4d891fd636
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2024-02-13 12:13:38 +0100

    step_time_lstm_tweaks

--- src/nn/archs/step_time_lstm.py
+++ src/nn/archs/step_time_lstm.py
@@ -1,10 +1,12 @@
 from typing import Optional
 
+import numpy as np
 import torch
 from torch import nn
 from torch.functional import F
 
 from src.nn.archs.lazy_fccn import LazyFCCN
+from src.nn.layers.attention import LazySelfAttention
 
 
 class StepTimeLSTM(nn.Module):
@@ -50,11 +52,20 @@ class StepTimeLSTM(nn.Module):
                                            device=device)
 
         memory_size = self.lstm_hidden_size * self.lstm_num_layers * 2
+
+        # self.memory_arranger = LazyFCCN(hidden_sizes=[memory_size * 4, memory_size * 2],
+        #                                 output_size=memory_size,
+        #                                 dropout_rate=self.lstm_dropout,
+        #                                 activation=torch.tanh,
+        #                                 device=device)
+
         self.memory_arranger = LazyFCCN(hidden_sizes=[memory_size * 8, memory_size * 4, memory_size * 2],
                                         output_size=memory_size,
                                         dropout_rate=self.lstm_dropout,
                                         activation=torch.tanh,
                                         device=device)
+
+        self.attention = None
 
     def forward(self, x, h0: Optional[torch.Tensor] = None, c0: Optional[torch.Tensor] = None):
         """
@@ -73,8 +84,8 @@ class StepTimeLSTM(nn.Module):
                              device=self.device)
 
         self.lstm.flatten_parameters()
+
         lstm_output, (hn, cn) = self.lstm(x, (h0, c0))
-        lstm_output = torch.cat([lstm_output, hn, cn])
 
         lstm_output = lstm_output.view(1, -1)
         out = self.output_interpreter(lstm_output)
@@ -82,11 +93,23 @@ class StepTimeLSTM(nn.Module):
 
         lstm_memory = torch.cat([h0, c0, hn, cn])
         lstm_memory = lstm_memory.view(1, -1)
+
+        # if self.attention is None:
+        #     self.attention = torch.nn.MultiheadAttention(lstm_memory.shape[1], 1, device=lstm_memory.device)
+        #
+        # attended, _ = self.attention(lstm_memory, lstm_memory, lstm_memory)
+        #
+        # lstm_memory = torch.cat([attended, lstm_memory], dim=1)
+
         lstm_memory_out = self.memory_arranger(lstm_memory)
         lstm_memory_out = F.tanh(lstm_memory_out)
 
         lstm_memory_out = lstm_memory_out.view(2 * self.lstm_num_layers, self.lstm_hidden_size)
 
         hn, cn = lstm_memory_out.split(self.lstm_num_layers)
+
+        # hn_flat, cn_flat = lstm_memory.split(h0.shape[0] * h0.shape[1], dim=1)[-2:]
+        # hn = hn_flat.reshape(hn.shape)
+        # cn = cn_flat.reshape(cn.shape)
 
         return out, (hn, cn)
