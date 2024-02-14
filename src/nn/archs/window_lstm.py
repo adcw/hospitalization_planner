7d3a7fb16a458ca973926b55d8307721931f7b3c
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2024-02-14 15:18:45 +0100

    window_lstm.py - remove unused functions, use last lstm element instead of average pooling

--- src/nn/archs/window_lstm.py
+++ src/nn/archs/window_lstm.py
@@ -1,46 +1,10 @@
 from typing import Optional, List
 
-import torch
 from torch import nn
 from torch.functional import F
 
 from src.nn.archs.lazy_fccn import LazyFCCN
 from src.nn.archs.lazy_mlc import MLConv, ConvLayerData as CLD
-from src.nn.archs.lazy_mlp import LazyMLP
-
-
-def masked_min(t: torch.Tensor,
-               m: Optional[torch.Tensor],
-               dim: int = -1
-               ):
-    if m is not None:
-        masked = t.clone()
-        masked[m.bool()] = torch.inf
-        t = masked
-
-    result, _ = torch.min(t, dim=dim)
-    return result
-
-
-def masked_max(t: torch.Tensor,
-               m: Optional[torch.Tensor],
-               dim: int = -1
-               ):
-    if m is not None:
-        masked = t.clone()
-        masked[m.bool()] = -torch.inf
-        t = masked
-
-    result, _ = torch.max(t, dim=dim)
-    return result
-
-
-def masked_range(t: torch.Tensor,
-                 m: torch.Tensor = None,
-                 dim: int = -1):
-    x_max = masked_max(t, m, dim=dim)
-    x_min = masked_min(t, m, dim=dim)
-    return abs(x_max - x_min)
 
 
 class WindowedConvLSTM(nn.Module):
@@ -91,14 +55,6 @@ class WindowedConvLSTM(nn.Module):
                             batch_first=True,
                             device=self.device, dropout=self.lstm_dropout)
 
-        # self.post_lstm_bnm = nn.LazyBatchNorm1d()
-
-        # self.mlp = LazyMLP(hidden_sizes=self.mlp_arch,
-        #                    output_size=self.output_size,
-        #                    dropout_rate=self.mpl_dropout,
-        #                    activation=self.mlp_activation
-        #                    )
-
         self.mlp = LazyFCCN(
             hidden_sizes=self.mlp_arch,
             output_size=self.output_size,
@@ -119,35 +75,9 @@ class WindowedConvLSTM(nn.Module):
         self.lstm.flatten_parameters()
         x_lstm, _ = self.lstm(x_conv)
 
-        # (batch, feat)
-        x_lstm = F.adaptive_avg_pool1d(x_lstm.permute(0, 2, 1), (1,)).view(x_lstm.size(0), -1)
-        x_lstm = F.tanh(x_lstm)
-
-        mlp_in = x_lstm
+        mlp_in = x_lstm[:, -1, :]
 
         x_mlp = self.mlp(mlp_in)
         output = F.sigmoid(x_mlp)
 
         return output
-"""
-    def forward(self, x):
-        x = x.permute(0, 2, 1)
-        x = self.conv1d(x)
-        x = F.selu(x)
-
-        x = self.dropout_conv(x)
-
-        x = x.permute(0, 2, 1)
-        output, _ = self.lstm(x)
-
-        # Uzyskanie ostatniego elementu z sekwencji
-        output = F.adaptive_avg_pool1d(output.permute(0, 2, 1), (1,)).view(output.size(0), -1)
-
-        output = self.dense(output)
-        output = F.selu(output)
-
-        output = self.classifier(output)
-        output = F.selu(output)
-
-        return output
-"""
