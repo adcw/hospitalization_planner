ef203462af0d863f292ee079cceeb286cc9dffdf
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2024-02-15 15:10:09 +0100

    Change layer depths and units in step_time_lstm.py

--- src/nn/archs/step_time_lstm.py
+++ src/nn/archs/step_time_lstm.py
@@ -1,12 +1,10 @@
 from typing import Optional
 
-import numpy as np
 import torch
 from torch import nn
 from torch.functional import F
 
 from src.nn.archs.lazy_fccn import LazyFCCN
-from src.nn.layers.attention import LazySelfAttention
 
 
 class StepTimeLSTM(nn.Module):
@@ -22,14 +20,14 @@ class StepTimeLSTM(nn.Module):
                  # FCCN params
                  fccn_arch: list[int] | None = None,
                  fccn_dropout_p: float = 0.15,
-                 fccn_activation=F.relu,
+                 fccn_activation=F.selu,
 
                  # Other
                  device: torch.device = 'cpu',
                  ):
         super(StepTimeLSTM, self).__init__()
 
-        fccn_arch = [32] * 5 if fccn_arch is None else fccn_arch
+        fccn_arch = [128, 64, 32] if fccn_arch is None else fccn_arch
         self.fccn_arch = fccn_arch
 
         self.fccn_dropout_p = fccn_dropout_p
@@ -53,16 +51,10 @@ class StepTimeLSTM(nn.Module):
 
         memory_size = self.lstm_hidden_size * self.lstm_num_layers * 2
 
-        # self.memory_arranger = LazyFCCN(hidden_sizes=[memory_size * 4, memory_size * 2],
-        #                                 output_size=memory_size,
-        #                                 dropout_rate=self.lstm_dropout,
-        #                                 activation=torch.tanh,
-        #                                 device=device)
-
-        self.memory_arranger = LazyFCCN(hidden_sizes=[memory_size * 8, memory_size * 4, memory_size * 2],
+        self.memory_arranger = LazyFCCN(hidden_sizes=[memory_size * 4, memory_size * 2],
                                         output_size=memory_size,
                                         dropout_rate=self.lstm_dropout,
-                                        activation=torch.tanh,
+                                        activation=torch.selu,
                                         device=device)
 
         self.attention = None
@@ -94,22 +86,11 @@ class StepTimeLSTM(nn.Module):
         lstm_memory = torch.cat([h0, c0, hn, cn])
         lstm_memory = lstm_memory.view(1, -1)
 
-        # if self.attention is None:
-        #     self.attention = torch.nn.MultiheadAttention(lstm_memory.shape[1], 1, device=lstm_memory.device)
-        #
-        # attended, _ = self.attention(lstm_memory, lstm_memory, lstm_memory)
-        #
-        # lstm_memory = torch.cat([attended, lstm_memory], dim=1)
-
         lstm_memory_out = self.memory_arranger(lstm_memory)
         lstm_memory_out = F.tanh(lstm_memory_out)
 
         lstm_memory_out = lstm_memory_out.view(2 * self.lstm_num_layers, self.lstm_hidden_size)
 
         hn, cn = lstm_memory_out.split(self.lstm_num_layers)
-
-        # hn_flat, cn_flat = lstm_memory.split(h0.shape[0] * h0.shape[1], dim=1)[-2:]
-        # hn = hn_flat.reshape(hn.shape)
-        # cn = cn_flat.reshape(cn.shape)
 
         return out, (hn, cn)
