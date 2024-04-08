130f2f96980875ec2c3505e9c289c87c086b7287
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2024-04-08 17:54:57 +0200

    change seed to 1234

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.session.model_manager import ModelManager
 from src.config.seeds import set_seed
 
 if __name__ == '__main__':
-    set_seed(9999)
+    set_seed(1234)
 
     manager = ModelManager(base_path='./runs', config_path="config.yaml")
     manager.start()
