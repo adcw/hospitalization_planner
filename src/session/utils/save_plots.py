16bdef0b1d17051f227827e2e65bbe99cd1c82fc
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2024-04-10 13:17:06 +0200

    Refactor save_plots, add graphviz saving

--- src/session/utils/save_plots.py
+++ src/session/utils/save_plots.py
@@ -23,7 +23,7 @@ def base_dir(path: Optional[str] = None):
     return _base_dir
 
 
-def save_plot(path: str, close: bool = True):
+def _prepare_path(path: str):
     global _base_dir
 
     if path is None or path == "":
@@ -37,6 +37,11 @@ def save_plot(path: str, close: bool = True):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
+    return save_path
+
+
+def save_plot(path: str, close: bool = True):
+    save_path = _prepare_path(path)
     plt.savefig(save_path)
 
     if close:
@@ -44,18 +49,11 @@ def save_plot(path: str, close: bool = True):
 
 
 def save_txt(path: str, txt: str):
-    global _base_dir
-
-    if path is None or path == "":
-        path = "."
-
-    save_path = os.path.join(_base_dir, path)
-    save_path = os.path.normpath(save_path)
-
-    folder_path = "\\".join(save_path.split("\\")[:-1])
-
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-
+    save_path = _prepare_path(path)
     with open(save_path, "w+") as file:
         file.write(txt)
+
+
+def save_viz(path: str, viz):
+    save_path = _prepare_path(path)
+    viz.save(save_path)
