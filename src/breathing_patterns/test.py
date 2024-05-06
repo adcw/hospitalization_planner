d4245704455869b6261dd669e7145cf25901c464
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2024-05-06 13:55:52 +0200

    Draw prediction result

--- src/breathing_patterns/test.py
+++ src/breathing_patterns/test.py
@@ -1,11 +1,17 @@
+from typing import List
+
+import matplotlib
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 from src.breathing_patterns.data.dataset import BreathingDataset
-from src.breathing_patterns.models.BreathingPatterModel import BreathingPatternModel
 from src.breathing_patterns.utils.clustering import learn_clusters
+from src.config.seeds import set_seed
+from src.error_analysis.extract import extract_seq_features
 from src.model_selection.stratified_sampling import stratified_sampling
 from src.session.utils.save_plots import base_dir
-from src.tools.dataframe_scale import scale
 from src.tools.iterators import windowed
 from src.tools.run_utils import get_run_path
 
@@ -14,30 +20,83 @@ GENERAL_SURFACTANT = 'GENERAL_SURFACTANT'
 FIO2 = 'FiO2'
 RTG_RDS = 'RTG_RDS'
 PO2 = 'po2'
+ANTYBIOTYK = 'ANTYBIOTYK'
+STERYD = 'STERYD'
 
 RESP_COLS = [
-    RESPIRATION, GENERAL_SURFACTANT, FIO2, RTG_RDS, PO2
+    PO2,
+    ANTYBIOTYK,
+    RTG_RDS,
+    STERYD,
+    FIO2,
+    RESPIRATION,
 ]
 
-DATASET_PATH = "../../bp_dataset_creation_runs/run_1/breathing_dataset.pkl"
+DATASET_PATH = "../../bp_dataset_creation_runs/run_3/breathing_dataset.pkl"
 RUN_PATH = "../../bp_test_runs"
 MODEL_PATH = "../../bp_train/run_3/model.pkl"
 
 MAX_PLOTS = 5
-MIN_X_WINDOW_RATE = 1
-STRIDE_RATE = 0.5
+MIN_X_WINDOW_RATE = 0.8
+STRIDE_RATE = 0.2
+
+
+def plot_breathing(
+        df: pd.DataFrame,
+        ranges: List[List],
+        y_labels_true
+):
+    stride = ranges[1][0] - ranges[0][0] if len(ranges) > 1 else 1
+    w_len = len(ranges[0])
+
+    n_lists = (int(np.ceil(w_len / stride)) + 1) if len(ranges) > 1 else 1
+
+    ranges_stairs = [[] for _ in range(n_lists)]
+    real_labels_stairs = [[] for _ in range(n_lists)]
+    pred_labels_stairs = [[] for _ in range(n_lists)]
+
+    for i, r in enumerate(ranges):
+        ranges_stairs[i % n_lists].append([r[int(w_len / 2)], r[-1]])
+        real_labels_stairs[i % n_lists].append(y_labels_true[i])
+        pred_labels_stairs[i % n_lists].append(1)
+
+    n_colors = max(y_labels_true + 1)
+    colors = matplotlib.colormaps['viridis'].resampled(n_colors)([x for x in range(n_colors)])
+
+    fig, ax = plt.subplots(len(df.columns) + 1, 1, figsize=(10, 14), sharex=True,
+                           gridspec_kw={'hspace': 0.5, 'height_ratios': [*([1] * len(df.columns)), 0.2 * n_lists]})
+
+    for i, column in enumerate(df.columns):
+        ax[i].plot(df[column])
+        ax[i].set_title(column)
+
+    for row_i, row in enumerate(ranges_stairs):
+        for rng_i, rng in enumerate(row):
+            real_label_color = colors[real_labels_stairs[row_i][rng_i]]
+            pred_label_color = colors[pred_labels_stairs[row_i][rng_i]]
+
+            ax[-1].hlines((len(ranges_stairs) - row_i) * 2, xmin=rng[0], xmax=rng[-1], linewidth=5,
+                          color=real_label_color)
+            ax[-1].hlines((len(ranges_stairs) - row_i) * 2 - 1, xmin=rng[0], xmax=rng[-1], linewidth=5,
+                          color=pred_label_color)
+            ax[-1].set_yticks([])
+
+    fig.suptitle("Breathing parameters")
+    plt.show()
+
 
 if __name__ == '__main__':
+    set_seed()
     run_path = get_run_path(RUN_PATH)
     base_dir(run_path)
 
     bd = BreathingDataset.read(DATASET_PATH)
-    model = BreathingPatternModel()
-    model.load(MODEL_PATH)
+    # model = BreathingPatternModel()
+    # model.load(MODEL_PATH)
 
     sequences = bd.test_sequences
-    scaled_sequences, scaler = scale(sequences)
-    kmed = learn_clusters(scaled_sequences, n_clusters=7, input_cols=[RESPIRATION], save_plots=False)
+    scaled_sequences = [pd.DataFrame(bd.scaler.transform(s), columns=s.columns) for s in sequences]
+    kmed = learn_clusters(scaled_sequences, n_clusters=12, input_cols=RESP_COLS, save_plots=False)
 
     # Perform quick test
     to_chose = min(MAX_PLOTS, len(bd.test_sequences))
@@ -48,7 +107,7 @@ if __name__ == '__main__':
 
         window_ranges = [w for w in
                          windowed(np.arange(scaled.shape[0]), window_size=bd.window_size * 2,
-                                  stride=int(STRIDE_RATE * bd.window_size))]
+                                  stride=max(1, int(STRIDE_RATE * bd.window_size)))]
 
         filtered_window_ranges = [wr for wr in window_ranges if len(wr) >= bd.window_size * (1 + MIN_X_WINDOW_RATE)]
 
@@ -57,6 +116,18 @@ if __name__ == '__main__':
 
         x_windows = [scaled.iloc[wr[:bd.window_size], :] for wr in filtered_window_ranges]
         y_windows = [scaled.iloc[wr[bd.window_size:], :] for wr in filtered_window_ranges]
+
+        x_window_features = extract_seq_features(x_windows, input_cols=RESP_COLS)
+        y_window_features = extract_seq_features(y_windows, input_cols=RESP_COLS)
+
+        x_classes = bd.kmed.predict(x_window_features)
+        y_classes = bd.kmed.predict(y_window_features)
+
+        plot_breathing(
+            sequences[i_seq][RESP_COLS],
+            ranges=filtered_window_ranges,
+            y_labels_true=y_classes,
+        )
 
         pass
 
