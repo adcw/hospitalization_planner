28528fb47c5d9dd6cdf490ac5e9a3c35fc61abb7
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2024-05-08 18:02:51 +0200

    Add weight to crossentropy loss

--- src/breathing_patterns/models/BreathingPatterModel.py
+++ src/breathing_patterns/models/BreathingPatterModel.py
@@ -1,27 +1,31 @@
 import contextlib
 import pickle
-from typing import Optional, Mapping, Any, List
+from typing import Optional, List
 
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import torch
+from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.model_selection import train_test_split
 from torch import nn, optim
-from torch.optim import Adam
 from tqdm import tqdm
 
 from src.breathing_patterns.data.dataset import BreathingDataset
+from src.model_selection.stratified import train_test_split_safe
 from src.nn.archs.lazy_mlc import ConvLayerData as CLD
 from src.nn.archs.window_lstm import WindowedConvLSTM
 from src.nn.callbacks.early_stopping import EarlyStopping
 from src.nn.callbacks.schedules import LrSchedule
 from src.session.utils.save_plots import save_plot, save_txt, base_dir
-from src.tools.iterators import batch_iter
+from src.tools.iterators import batch_iter, shuffled
 
-from matplotlib import pyplot as plt
 
-import seaborn as sns
+def calculate_class_weights(class_counts, strength=1.0):
+    weights = 1 / np.array(class_counts)
+    weights = (weights ** strength) / np.sum(weights ** strength)
+
+    return weights
 
 
 class BreathingPatternModel:
@@ -43,6 +47,11 @@ class BreathingPatternModel:
 
         pbar = tqdm(desc="Forwarding", total=len(xs))
         total_loss = 0.0
+
+        sh_ind = shuffled([i for i in range(len(xs))], p=0.2)
+        xs = [xs[i] for i in sh_ind]
+        ys = [ys[i] for i in sh_ind]
+
         for xs_batch, ys_batch in batch_iter(xs, ys, batch_size=batch_size):
             xs_batch = torch.stack(xs_batch).to(self.device)
             ys_batch = torch.Tensor(ys_batch).long().to(self.device)
@@ -69,14 +78,22 @@ class BreathingPatternModel:
 
     def __setup_net(self):
         cldata = [
-            CLD(channels=16, kernel_size=3, activation=nn.SELU),
-            CLD(channels=32, kernel_size=3, activation=nn.SELU),
             CLD(channels=64, kernel_size=3, activation=nn.SELU),
+            CLD(channels=128, kernel_size=3, activation=nn.SELU),
+            CLD(channels=256, kernel_size=3, activation=nn.SELU),
+            CLD(channels=512, kernel_size=3, activation=nn.SELU)
         ]
 
         self.__net = WindowedConvLSTM(n_attr=self.n_attr,
                                       output_size=self.n_classes,
                                       conv_layers_data=cldata,
+
+                                      lstm_hidden_size=256,
+                                      lstm_layers=2,
+                                      lstm_dropout=0.2,
+                                      mlp_dropout=0.2,
+                                      mlp_arch=[128, 64, 32],
+
                                       final_activation=None) \
             .to(self.device)
 
@@ -102,17 +119,19 @@ class BreathingPatternModel:
         self.n_attr = dataset.xs[0].shape[1]
         self.n_classes = len(np.unique(dataset.ys_classes))
 
-        self.__setup_net()
+        xs_train, ys_train, xs_val, ys_val = train_test_split_safe(dataset.xs, dataset.ys_classes, test_size=0.2,
+                                                                   stratify=dataset.ys_classes)
 
-        self.__criterion = nn.CrossEntropyLoss()
-        self.__optimizer = optim.Adam(self.__net.parameters(), weight_decay=0.001, lr=0.001)
+        train_classes_counts = np.unique(ys_train, return_counts=True)[1]
+        weight = torch.Tensor(calculate_class_weights(train_classes_counts, strength=1)).to(self.device)
+
+        self.__setup_net()
+        self.__criterion = nn.CrossEntropyLoss(weight=weight)
+        self.__optimizer = optim.Adam(self.__net.parameters(), weight_decay=0.01, lr=0.0001)
 
         early_stopping = EarlyStopping(self.__net, patience=es_patience)
 
-        lr_schedule = LrSchedule(optimizer=self.__optimizer, early_stopping=early_stopping, verbose=2)
-
-        xs_train, xs_val, ys_train, ys_val = train_test_split(dataset.xs, dataset.ys_classes, test_size=0.2,
-                                                              stratify=dataset.ys_classes)
+        lr_schedule = LrSchedule(optimizer=self.__optimizer, early_stopping=early_stopping, verbose=1)
 
         print(f"Saving training results to {base_dir()}")
 
