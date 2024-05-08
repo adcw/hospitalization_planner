90dcf0bc82a32f9097be1dceab85ca127dc1abba
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2024-05-08 18:01:45 +0200

    Train for 400 epochs, esp=20

--- src/breathing_patterns/train.py
+++ src/breathing_patterns/train.py
@@ -1,19 +1,22 @@
 from src.breathing_patterns.data.dataset import BreathingDataset
 from src.breathing_patterns.models.BreathingPatterModel import BreathingPatternModel
+from src.config.seeds import set_seed
 from src.session.utils.save_plots import base_dir
 from src.tools.run_utils import get_run_path
 
-DATASET_PATH = "../../bp_dataset_creation_runs/run_3/breathing_dataset.pkl"
+DATASET_PATH = "../../bp_dataset_creation_runs/run_2/breathing_dataset.pkl"
 RUN_PATH = "../../bp_train_runs"
 
 if __name__ == '__main__':
+    set_seed()
     run_path = get_run_path(RUN_PATH)
+    print(f"Current run path: {run_path}")
     base_dir(run_path)
 
     bd = BreathingDataset.read(DATASET_PATH)
     model = BreathingPatternModel()
 
-    model.fit(bd, batch_size=16, n_epochs=100)
+    model.fit(bd, batch_size=16, n_epochs=400, es_patience=20)
     model.dump(f"{base_dir()}/model.pkl")
 
     pass
