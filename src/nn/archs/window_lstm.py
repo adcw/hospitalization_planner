4692bb21e9b9e183af73002ab90365fdc6e8d443
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2024-05-30 10:19:19 +0200

    Add with_conv and with_lstm control arguments

--- src/nn/archs/window_lstm.py
+++ src/nn/archs/window_lstm.py
@@ -14,10 +14,12 @@ class WindowedConvLSTM(nn.Module):
                  output_size: int,
 
                  # conv
+                 with_conv: bool = True,
                  conv_layers_data: Optional[List[CLD]] = None,
                  conv_channel_dropout: float = 0,
 
                  # LSTM
+                 with_lstm: bool = False,
                  lstm_hidden_size: int = 128,
                  lstm_layers: int = 2,
                  lstm_dropout: float = 0.2,
@@ -45,6 +47,9 @@ class WindowedConvLSTM(nn.Module):
         self.mlp_dropout = mlp_dropout
         self.mlp_activation = mlp_activation
 
+        self.with_lstm = with_lstm
+        self.with_conv = with_conv
+
         self.final_activation = final_activation
 
         self.cldata = conv_layers_data or [
@@ -53,14 +58,20 @@ class WindowedConvLSTM(nn.Module):
             CLD(channels=32, kernel_size=3, activation=nn.SELU),
         ]
 
-        self.mlconv = MLConv(input_size=n_attr, conv_layers_data=self.cldata, dropout_rate=conv_channel_dropout)
+        if self.with_conv:
+            self.mlconv = MLConv(input_size=n_attr, conv_layers_data=self.cldata, dropout_rate=conv_channel_dropout)
+        else:
+            self.mlconv = None
 
-        # self.lstm = nn.LSTM(input_size=self.cldata[-1].channels,
-        #                     hidden_size=self.lstm_hidden_size,
-        #                     num_layers=self.lstm_layers,
-        #                     batch_first=True,
-        #                     device=self.device,
-        #                     dropout=self.lstm_dropout)
+        if self.with_lstm:
+            self.lstm = nn.LSTM(input_size=self.cldata[-1].channels if self.with_conv else n_attr,
+                                hidden_size=self.lstm_hidden_size,
+                                num_layers=self.lstm_layers,
+                                batch_first=True,
+                                device=self.device,
+                                dropout=self.lstm_dropout)
+        else:
+            self.lstm = None
 
         self.mlp = LazyFCCN(
             hidden_sizes=self.mlp_arch,
@@ -72,26 +83,65 @@ class WindowedConvLSTM(nn.Module):
     def forward(self, x, x_c):
         # x.shape = (batch, seq, feat)
 
-        x_perm = x.permute(0, 2, 1)
-        # (batch, feat, seq)
+        if self.mlconv is not None:
+            x_perm = x.permute(0, 2, 1)
+            # (batch, feat, seq)
 
-        if x_c is not None:
-            x_c_2 = torch.reshape(x_c,(x_c.shape[0], x_c.shape[1], 1))
-            x_c_2 = x_c_2.repeat(1, 1, x.shape[1])
-            x_perm = torch.concat((x_perm, x_c_2), dim=1)
+            if x_c is not None:
+                x_c_2 = torch.reshape(x_c, (x_c.shape[0], x_c.shape[1], 1))
+                x_c_2 = x_c_2.repeat(1, 1, x.shape[1])
+                x_perm = torch.concat((x_perm, x_c_2), dim=1)
 
-        x_conv = self.mlconv(x_perm)
-        x_conv = x_conv.permute(0, 2, 1)
-        # (batch, seq, feat)
+            x_conv = self.mlconv(x_perm)
+            x = x_conv.permute(0, 2, 1)
 
-        # self.lstm.flatten_parameters()
-        # x_lstm, _ = self.lstm(x_conv)
-        #
-        # mlp_in = x_lstm[:, -1, :]
+        if self.lstm is not None:
+            self.lstm.flatten_parameters()
+            x_lstm, _ = self.lstm(x)
+            x = x_lstm[:, -1, :]
 
-        output = self.mlp(x_conv.flatten(-2))
+        if x.ndim > 2:
+            x = x.flatten(-2)
+
+        output = self.mlp(x)
 
         if self.final_activation is not None:
             output = self.final_activation(output)
 
         return output
+
+
+if __name__ == '__main__':
+    # Ustalanie parametrów modelu
+    n_attr = 10
+    output_size = 6
+    batch_size = 4
+    seq_length = 20
+
+    cldata = [
+        CLD(channels=64, kernel_size=3, activation=nn.SELU),
+        CLD(channels=128, kernel_size=3, activation=nn.SELU),
+        # CLD(channels=256, kernel_size=3, activation=nn.SELU),
+        # CLD(channels=512, kernel_size=3, activation=nn.SELU),
+    ]
+
+    # Tworzenie instancji modelu
+    model = WindowedConvLSTM(
+        n_attr=n_attr,
+        output_size=output_size,
+        device='cpu',
+
+        with_conv=False,
+        with_lstm=False,
+
+        conv_layers_data=cldata
+    )
+
+    # Generowanie losowego tensora wejściowego
+    x = torch.randn(batch_size, seq_length, n_attr)
+    # x_c = torch.randn(batch_size, n_attr)
+
+    # Przepuszczanie tensora przez model
+    output = model(x, None)
+
+    pass
